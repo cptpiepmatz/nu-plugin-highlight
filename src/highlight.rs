64b43a62ed61057b1859316b93d49d98af4d8a9b
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-06-29 10:57:15 +0200

    removed unused function

--- src/highlight.rs
+++ src/highlight.rs
@@ -74,18 +74,3 @@ impl Highlighter {
             .collect::<String>().trim().to_owned()
     }
 }
-
-pub fn highlight_do_something(
-    param: Option<Spanned<String>>,
-    val: &str,
-    value_span: Span
-) -> Result<Value, LabeledError> {
-    let a_val = match param {
-        Some(p) => format!("Hello, {}! with value: {}", p.item, val),
-        None => format!("Hello, Default! with value: {}", val)
-    };
-    Ok(Value::String {
-        val: a_val,
-        span: value_span
-    })
-}
